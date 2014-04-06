d8f86fbb43997697a2f8da20b930c2b02ee137e6
edsonarthur.sp <edsonarthur.sp@7d67db08-3fb5-d1a1-eeb4-b136d5ff8eda>
2014-04-06 16:40:59 +0000

    Criação das classes "AssuntoController", "DisciplinaController" e alteração da "ProfessorController".

---
+++ trunk/ProjectEnsino/src/br/fatec/controller/AssuntoController.java
@@ -0,0 +1,75 @@
+package br.fatec.controller;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import br.fatec.dao.AssuntoDAO;
+import br.fatec.model.Assunto;
+
+@ManagedBean(name="assuntoController")
+@SessionScoped
+
+public class AssuntoController {
+	private List<Assunto> assuntos;
+	private AssuntoDAO assuntoDao;
+	private Assunto currentAssunto;
+	private Assunto newAssunto;
+	
+	public AssuntoController()
+	{
+		
+	}
+	
+	@PostConstruct
+	public void preparaDados()
+	{
+		this.assuntoDao = new AssuntoDAO();
+		this.newAssunto = new Assunto();
+		this.currentAssunto = new Assunto();
+	}
+
+	public List<Assunto> getAssuntos() {
+		return assuntos;
+	}
+
+	public void setAssuntos(List<Assunto> assuntos) {
+		this.assuntos = assuntos;
+	}
+
+	public AssuntoDAO getAssuntoDao() {
+		return assuntoDao;
+	}
+
+	public void setAssuntoDao(AssuntoDAO assuntoDao) {
+		this.assuntoDao = assuntoDao;
+	}
+
+	public Assunto getCurrentAssunto() {
+		return currentAssunto;
+	}
+
+	public void setCurrentAssunto(Assunto currentAssunto) {
+		this.currentAssunto = currentAssunto;
+	}
+
+	public Assunto getNewAssunto() {
+		return newAssunto;
+	}
+
+	public void setNewAssunto(Assunto newAssunto) {
+		this.newAssunto = newAssunto;
+	}
+	
+	public void cadastrar()
+	{	
+		if (assuntoDao.inserir(this.newAssunto))
+			System.out.println("Assunto inserido com sucesso!");
+		else
+			System.out.println("Erro na inserção!");
+		
+		this.newAssunto = new Assunto();
+	}
+}
--- trunk/ProjectEnsino/src/br/fatec/controller/ProfessorController.java
+++ trunk/ProjectEnsino/src/br/fatec/controller/ProfessorController.java
@@ -1,5 +1,75 @@
 package br.fatec.controller;
 
-public class ProfessorController {
+import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import br.fatec.dao.ProfessorDAO;
+import br.fatec.model.Professor;
+
+@ManagedBean(name="professorController")
+@SessionScoped
+
+public class ProfessorController {
+	private List<Professor> professores;
+	private ProfessorDAO professorDao;
+	private Professor currentProfessor;
+	private Professor newProfessor;
+	
+	public ProfessorController()
+	{
+		
+	}
+	
+	@PostConstruct
+	public void preparaDados()
+	{
+		this.professorDao = new ProfessorDAO();
+		this.newProfessor = new Professor();
+		this.currentProfessor = new Professor();
+	}
+
+	public List<Professor> getProfessores() {
+		return professores;
+	}
+
+	public void setProfessores(List<Professor> professores) {
+		this.professores = professores;
+	}
+
+	public ProfessorDAO getProfessorDao() {
+		return professorDao;
+	}
+
+	public void setProfessorDao(ProfessorDAO professorDao) {
+		this.professorDao = professorDao;
+	}
+
+	public Professor getCurrentProfessor() {
+		return currentProfessor;
+	}
+
+	public void setCurrentProfessor(Professor currentProfessor) {
+		this.currentProfessor = currentProfessor;
+	}
+
+	public Professor getNewProfessor() {
+		return newProfessor;
+	}
+
+	public void setNewProfessor(Professor newProfessor) {
+		this.newProfessor = newProfessor;
+	}
+	
+	public void cadastrar()
+	{	
+		if (professorDao.inserir(this.newProfessor))
+			System.out.println("Professor inserido com sucesso!");
+		else
+			System.out.println("Erro na inserção!");
+		
+		this.newProfessor = new Professor();
+	}
 }
