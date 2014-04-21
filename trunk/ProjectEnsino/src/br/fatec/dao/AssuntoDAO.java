8909a6db2e65773ef06466a2d070f9be19ad2fdb
renatodanielss <renatodanielss@7d67db08-3fb5-d1a1-eeb4-b136d5ff8eda>
2014-04-21 23:34:31 +0000

    Atualização de métdos para popular combobox assunto dinâmicamente na página Textobase.xhtml

--- trunk/ProjectEnsino/src/br/fatec/dao/AssuntoDAO.java
+++ trunk/ProjectEnsino/src/br/fatec/dao/AssuntoDAO.java
@@ -1,7 +1,6 @@
 package br.fatec.dao;
 
 import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -55,10 +54,10 @@ public class AssuntoDAO {
 	}
 	
 	@SuppressWarnings("unchecked")
-	public List<Assunto> listar(int iddisciplina_assunto)
-	{		
+	public List<Assunto> listarListener(int valor)
+	{
 		List<Assunto> listAssunto = null;
-		String query = "select * from tbl_assunto where iddisciplina_assunto = " + iddisciplina_assunto;
+		String query = "select * from tbl_assunto where iddisciplina_assunto = " + valor;
 		
 		try
 		{
