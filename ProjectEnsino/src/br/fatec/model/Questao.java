1585776b599c33f772d508d39f38fb2f0b308660
renatodanielss <renatodanielss@7d67db08-3fb5-d1a1-eeb4-b136d5ff8eda>
2014-05-12 07:28:33 +0000

    Implementação de questão e alternativas

---
+++ ProjectEnsino/src/br/fatec/model/Questao.java
@@ -0,0 +1,106 @@
+package br.fatec.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tbl_questao")
+public class Questao {
+	@Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "QUESTAO_ID")
+	@SequenceGenerator(name="QUESTAO_ID", sequenceName="QUESTAO_SEQ", allocationSize=1)
+	@Column(name="id_questao")
+	private int id_questao;
+	
+	@OneToOne(cascade = CascadeType.ALL, optional = false, fetch = FetchType.EAGER, orphanRemoval = true)
+    @PrimaryKeyJoinColumn
+	private Textobase textobase_questao;
+	
+	@OneToOne(cascade = CascadeType.ALL, optional = false, fetch = FetchType.EAGER, orphanRemoval = true)
+    @PrimaryKeyJoinColumn
+	private Autor autor_questao;
+	
+	@Column(name="enunciado_questao")
+	private String enunciado_questao;
+	
+	@OneToOne(cascade = CascadeType.ALL, optional = false, fetch = FetchType.EAGER, orphanRemoval = true)
+    @PrimaryKeyJoinColumn
+	private Disciplina disciplina_questao;
+	
+	@OneToOne(cascade = CascadeType.ALL, optional = false, fetch = FetchType.EAGER, orphanRemoval = true)
+    @PrimaryKeyJoinColumn
+	private Assunto assunto_questao;
+	
+	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+    @JoinColumn(name="id_alternativa")
+	private List<Alternativa> alternativas_questao;
+
+	public int getId_questao() {
+		return id_questao;
+	}
+
+	public void setId_questao(int id_questao) {
+		this.id_questao = id_questao;
+	}
+
+	public Textobase getTextobase_questao() {
+		return textobase_questao;
+	}
+
+	public void setTextobase_questao(Textobase textobase_questao) {
+		this.textobase_questao = textobase_questao;
+	}
+
+	public Autor getAutor_questao() {
+		return autor_questao;
+	}
+
+	public void setAutor_questao(Autor autor_questao) {
+		this.autor_questao = autor_questao;
+	}
+
+	public String getEnunciado_questao() {
+		return enunciado_questao;
+	}
+
+	public void setEnunciado_questao(String enunciado_questao) {
+		this.enunciado_questao = enunciado_questao;
+	}
+
+	public Disciplina getDisciplina_questao() {
+		return disciplina_questao;
+	}
+
+	public void setDisciplina_questao(Disciplina disciplina_questao) {
+		this.disciplina_questao = disciplina_questao;
+	}
+
+	public Assunto getAssunto_questao() {
+		return assunto_questao;
+	}
+
+	public void setAssunto_questao(Assunto assunto_questao) {
+		this.assunto_questao = assunto_questao;
+	}
+
+	public List<Alternativa> getAlternativas_questao() {
+		return alternativas_questao;
+	}
+
+	public void setAlternativas_questao(List<Alternativa> alternativas_questao) {
+		this.alternativas_questao = alternativas_questao;
+	}
+}
